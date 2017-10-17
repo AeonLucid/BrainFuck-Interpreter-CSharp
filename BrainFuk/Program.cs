c24dc50e6ebdb3e5a76d04ebae1eb82b2595ac87
AeonLucid <aeonlucid@outlook.com>
2017-10-17 09:59:29 +0200

    Add project files.

---
+++ BrainFuk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BrainFuk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
